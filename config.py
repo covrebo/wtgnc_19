6e502f059bd39bff2e826a3637d5e47113e9fd03
Chris Ovrebo <chris.ovrebo@gmail.com>
2019-02-25 08:38:51 -0600

    Made changes to prepare for Heroku deployment

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = os.environ.get('ADMINS')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
